4838f62efffc4b9570cfdbbaed8c187a8f95ff4b
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-03-27 08:22:44 +0530

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app = express();
 
 app.get("/",(req,res)=>{
 res.json({
-    message:"Hello from node app",
+    message:"Hello from node app ",
     date: new Date(),
     version: process.env.npm_package_version,
     os: os.platform()
